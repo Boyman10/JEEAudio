d46f5fd39d43098f2e762539f1578f1e87ab5ae2
Eddy Mio <contact@remotehireman.fr>
2018-04-09 19:28:11 +0200

    ADding Traduction entity to hold our translated strings along with other parameters

---
+++ Subtitlor/src/com/subtitlor/model/entity/Traduction.java
@@ -0,0 +1,59 @@
+package com.subtitlor.model.entity;
+
+import java.util.ArrayList;
+
+/**
+ * Class holding a traduction of a video
+ * @author bob
+ * @version 1.0
+ */
+public class Traduction {
+
+	private int id ;
+	private String sequence;
+	private ArrayList<String> strings;
+	private String language;
+	
+	/**
+	 * Constructor class
+	 * @param id
+	 * @param seq
+	 * @param language
+	 * @param strs
+	 */
+	public Traduction(int id, String seq, String language, ArrayList<String> strs) {
+		
+		this.id = id;
+		this.sequence = seq;
+		this.language = language;
+		this.strings = strs;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSequence() {
+		return sequence;
+	}
+	public void setSequence(String sequence) {
+		this.sequence = sequence;
+	}
+	public ArrayList<String> getStrings() {
+		return strings;
+	}
+	public void setStrings(ArrayList<String> strings) {
+		this.strings = strings;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+	
+	
+}
