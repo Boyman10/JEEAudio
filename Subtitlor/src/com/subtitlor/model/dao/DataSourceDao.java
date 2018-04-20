5b2dc7f6ff25906ec2cb7950eef3533472b4bd70
Eddy Mio <contact@remotehireman.fr>
2018-04-20 15:24:38 +0200

    Successfully loading DataSource after fixing parameters in context.xml in WebContent/META-INF

--- Subtitlor/src/com/subtitlor/model/dao/DataSourceDao.java
+++ Subtitlor/src/com/subtitlor/model/dao/DataSourceDao.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
+import javax.sql.DataSource;
 
 import com.subtitlor.model.entity.Traduction;
 
